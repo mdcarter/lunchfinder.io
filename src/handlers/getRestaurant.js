f5b37ba2667b6fcd181d19e599aee0751703075f
Maxime Dehaye <maxime.dehaye@gmail.com>
2017-08-21 15:06:30 +0200

    Add query modules to make call clearer

--- src/handlers/getRestaurant.js
+++ src/handlers/getRestaurant.js
@@ -1,18 +1,21 @@
 import Request from './../utils/Request';
+import Query from 'query-string';
 
 export default async context => {
   context.setState({ status: { fetchingData: true } });
-  const data = await Request.fetch(
-    `https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259
-      &ll=${context.state.latitude},${context.state.longitude}
-      &radius=${context.state.radius}
-      &limit=20
-      &intent=browse
-      &client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT}
-      &client_secret=${process.env.REACT_APP_FOURSQUARE_SECRET}
-      &v=20170815`
-  );
 
+  const params = {
+    categoryId: process.env.REACT_APP_FOURSQUARE_FOOD_CATEGORY,
+    ll: context.state.latitude + ',' + context.state.longitude,
+    radius: context.state.radius,
+    limit: 25,
+    intent: 'browse',
+    client_id: process.env.REACT_APP_FOURSQUARE_CLIENT,
+    client_secret: process.env.REACT_APP_FOURSQUARE_SECRET,
+    v: '20170815'
+  };
+
+  const data = await Request.fetch(`${process.env.REACT_APP_FOURSQUARE_BASE_URL}venues/search?${Query.stringify(params)}`);
   if (data && data.response && data.response.venues) {
     context.setState({
       restaurant: data.response.venues[Math.floor(Math.random() * data.response.venues.length)],
