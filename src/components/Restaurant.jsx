b9322524a69d31b155542e2b5a87cd58e0d4ce29
Maxime Dehaye <maxime.dehaye@gmail.com>
2017-09-11 18:04:17 +0200

    Add first test with Google Map display

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Reflux from 'reflux';
+import GoogleMapReact from 'google-map-react';
 
+import Marker from './Marker';
 import Store from './../Store';
 import Actions from './../Actions';
+import Theme from './../styles/map';
 
 import './../styles/Restaurant.css';
 
@@ -25,16 +28,31 @@ export default class Restaurant extends Reflux.Component {
       return null;
     }
 
+    const coords = { lat: this.state.restaurant.location.lat, lng: this.state.restaurant.location.lng };
+    const options = {
+      disableDefaultUI: true,
+      mapTypeControl: false,
+      draggable: false,
+      styles: Theme
+    };
+
     return (
       <div className="page-restaurant">
-        <h2>Restaurant proposé: {this.state.restaurant.name}</h2>
-        <button onClick={Actions.getRestaurant}>Un autre svp</button>
-        <p>
-          {this.state.restaurant.location.address} ({this.state.restaurant.categories[0].name})
-        </p>
-        <pre>
-          <code>{JSON.stringify(this.state.restaurant, undefined, 1)}</code>
-        </pre>
+        <div className="restaurant">
+          <h2>Restaurant proposé: {this.state.restaurant.name}</h2>
+          <button onClick={Actions.getRestaurant}>Un autre svp</button>
+          <p>
+            {this.state.restaurant.location.address} ({this.state.restaurant.categories[0].name})
+          </p>
+          <pre>
+            <code>{JSON.stringify(this.state.restaurant, undefined, 1)}</code>
+          </pre>
+        </div>
+        <div className="map">
+          <GoogleMapReact center={coords} defaultZoom={16} options={options}>
+            <Marker lat={this.state.restaurant.location.lat} lng={this.state.restaurant.location.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
