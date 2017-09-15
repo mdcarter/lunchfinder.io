15821dbdfd573e584bb01157364df3daa6e7e3dc
Maxime Dehaye <maxime.dehaye@gmail.com>
2017-09-15 18:54:03 +0200

    Add empty first test of handler

---
+++ src/tests/handlers/setDirectionsLink.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import setDirectionsLink from './../../handlers/setDirectionsLink';
+
+describe('setDirectionsLink handler', () => {
+  it('exists', () => {
+    console.log('yay');
+  });
+});
