f40b7f9651b5871c47427aa89b7bc108fa35fe5f
Maxime Dehaye <maxime.dehaye@gmail.com>
2017-09-15 18:58:54 +0200

    Add coverage for setDirectionsLink handler

--- src/tests/handlers/setDirectionsLink.test.js
+++ src/tests/handlers/setDirectionsLink.test.js
@@ -3,8 +3,24 @@ import sinon from 'sinon';
 
 import setDirectionsLink from './../../handlers/setDirectionsLink';
 
+let context = {
+  setState(state) {},
+  state: {
+    address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
+    restaurant: {
+      name: 'Enishi',
+      location: {
+        address: '67 Rue Labat',
+        city: 'Paris',
+        country: 'France'
+      }
+    }
+  }
+};
+
 describe('setDirectionsLink handler', () => {
-  it('exists', () => {
-    console.log('yay');
+  it('set directions link in state', () => {
+    setDirectionsLink(context);
+    expect(context.state.restaurant.directions).not.equal(false);
   });
 });
