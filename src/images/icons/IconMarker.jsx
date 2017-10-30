67cc08a9ce74e7b04b7652c75c53b9ef09ce6be9
Maxime Dehaye <mdehaye@viadeoteam.com>
2017-10-30 14:19:25 +0100

    Change icons from svg to react components

---
+++ src/images/icons/IconMarker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => (
+  <svg width={45} height={64} viewBox="0 0 45 64" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path
+        fill="#31BB9A"
+        fillRule="nonzero"
+        d="M22.5 0C10 0 0 10 0 22.4 0 39.2 22.5 64 22.5 64S45 39.2 45 22.4C45 10 35 0 22.5 0zm0 30.4c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"
+      />
+      <path d="M-16-6h77v77h-77z" />
+    </g>
+  </svg>
+);
