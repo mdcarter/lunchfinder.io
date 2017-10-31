1c9aaaea3173147f3a1bfb01047d28dfa8df97d2
Maxime Dehaye <mdehaye@viadeoteam.com>
2017-10-31 15:38:36 +0100

    Add coverage on Address

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,20 +1,45 @@
 import 'raf/polyfill';
-import React from 'react';
+import React, { Component } from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import GoogleMapReact from 'google-map-react';
 
 configure({ adapter: new Adapter() });
 
-jest.mock('react-places-autocomplete', () => {
-  return props => {
-    return <div className="react-places-autocomplete" />;
+jest.mock('google-map-react');
+
+GoogleMapReact.mockImplementation(() => {
+  return {
+    render: () => {
+      return <div className="google-map-react" />;
+    }
   };
 });
 
-jest.mock('google-map-react', () => {
-  return props => {
-    return <div className="google-map-react" />;
+jest.mock('react-places-autocomplete');
+
+PlacesAutocomplete.mockImplementation(() => {
+  return {
+    render: () => {
+      return <div className="react-places-autocomplete" />;
+    }
   };
+});
+
+geocodeByAddress.mockImplementation(() => {
+  return new Promise(resolve => {
+    resolve(['address']);
+  });
+});
+
+getLatLng.mockImplementation(() => {
+  return new Promise(resolve => {
+    resolve({
+      lat: 1,
+      lng: 2
+    });
+  });
 });
 
 window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ json: () => [] }));
--- src/tests/components/Address.test.js
+++ src/tests/components/Address.test.js
@@ -17,8 +17,24 @@ describe('<Address />', () => {
     sandbox.restore();
   });
 
-  it('should render a PlacesAutocomplete component', () => {
+  it('should always renders a div', () => {
     const wrapper = mount(<Address />);
     expect(wrapper.find('div.react-places-autocomplete').length).toBeGreaterThan(0);
+  });
+
+  it('should call Store and Google API methods when selectAddress method is called', async () => {
+    let updateAddress = sinon.spy();
+    let updateLocation = sinon.spy();
+
+    const props = {
+      updateAddress: updateAddress,
+      updateLocation: updateLocation
+    };
+
+    const wrapper = mount(<Address {...props} />);
+    let call = await wrapper.instance().selectAddress('test');
+
+    expect(updateAddress.calledOnce).toEqual(true);
+    expect(updateLocation.calledOnce).toEqual(true);
   });
 });
