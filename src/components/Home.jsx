b03b6723c9b9d3cc36e35407c9912411981bbf81
mdcarter <maxime.dehaye@gmail.com>
2018-08-25 02:00:14 +0200

    Small PWA changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,10 +26,11 @@ export default class Home extends Reflux.Component {
           <Logo />
           <h2>« Where do we eat today ? »</h2>
           <h3>Ask LunchFinder !</h3>
-          <h4>This app helps you and your co-workers find a random place to eat everyday.</h4>
+          <h4>This app helps you and your co-workers find a random place to eat everyday</h4>
           <form className="form">
             <Address address={this.state.address || ''} updateAddress={Actions.setCurrentAddress} updateLocation={Actions.setCurrentLocation} />
             <button
+              aria-label="Find a restaurant"
               className={this.state.retrievingLocation ? 'find btn-loading' : 'find'}
               type="button"
               onClick={Actions.selectRestaurant}
@@ -39,7 +40,7 @@ export default class Home extends Reflux.Component {
               {this.state.retrievingLocation !== false && <Icons name="Loader" />}
             </button>
           </form>
-          <button className="locate" onClick={Actions.getCurrentLocation}>
+          <button className="locate" aria-label="Use my location" onClick={Actions.getCurrentLocation}>
             <Icons name="Locate" /> My location
           </button>
         </section>
